32859eccc8717c74ce05d36c0e7fd2265e332246
Saurav Deb Purkayastha <sauravdebp@gmail.com>
2016-01-21 23:44:14 +0530

    CreateSecurity Proc finalised.

---
+++ Queries/DDL/SecurityAttributes.sql
@@ -0,0 +1,33 @@
+USE [SecurityMaster]
+GO
+
+/****** Object:  Table [dbo].[SecurityAttributes]    Script Date: 21-Jan-16 10:54:20 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[SecurityAttributes](
+	[AttributeId] [nchar](10) NULL,
+	[SecurityTypeId] [int] NULL,
+	[AttributeDisplayName] [varchar](100) NULL,
+	[AttributeRealName] [varchar](50) NULL,
+	[AttributeDataType] [varchar](50) NULL,
+	[AttributeLength] [int] NULL,
+	[LastModifiedBy] [varchar](50) NULL,
+	[LastModifiedOn] [datetime] NULL,
+	[CreatedBy] [varchar](50) NULL,
+	[CreatedOn] [datetime] NULL,
+	[IsActive] [bit] NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
